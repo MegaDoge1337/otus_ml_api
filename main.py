10e06cd0ec99dc3c48268b2e7c0b8fd66543fcc2
Aleksandr <gggols32@gmail.com>
2024-12-12 21:13:56 +0300

    Add fastapi endpoint with features validation

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from ml_models import AnimalClassificationModel
+
+app = FastAPI()
+
+class Features(BaseModel):
+    paws_count: int
+    has_fur: int
+    mammal: int
+
+@app.post("/make_inference")
+def make_inference(features: Features):
+    result = AnimalClassificationModel.make_inference(paws_count=features.paws_count,
+                                                      has_fur=features.has_fur,
+                                                      mammal=features.mammal)
+    return {"result": result}
